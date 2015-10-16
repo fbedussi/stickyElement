3d48c5f409831ef586ed37366eba35b919de340b
Francesco Bedussi <francesco@francescobedussi.it>
2015-10-16 18:02:20 +0200

    Init

--- stickyElement.js
+++ stickyElement.js
@@ -1,8 +1,10 @@
 (function () {
+    "use strict";
+    
     function setup(element, yOffset) {
 
-        var $document = $(document),
-            $element = $(element),
+        var docBody = document.getElementsByTagName('body')[0],
+            el = document.querySelector(element),
             elementHeight,
             pos,
             newPos,
@@ -11,28 +13,28 @@
             elementOriginalAbsoluteTop,
             elementOriginalRelativeTop,
             containerAvailableHeight,
-            $parentElement = $element.parent();
+            parentEl = el.parentElement;
         
         if (typeof yOffset === "undefined") {
             yOffset = 0;
         }
 
-        function getElementCssTop() {
-            return $element.css('top').replace("auto", "0").replace("px", "");
+        function getElementCssTop(el) {
+            return window.getComputedStyle(el).getPropertyValue('top').replace("auto", "0").replace("px", "");
         }
 
         function initMeasures() {
-            elementHeight = $element.outerHeight(true);
-            elementOriginalAbsoluteTop = $element.offset().top - getElementCssTop();
-            elementOriginalRelativeTop = $element.position().top - $parentElement.css('padding-top').replace("px", "") - getElementCssTop();
+            elementHeight = el.offsetHeight;
+            elementOriginalAbsoluteTop = el.getBoundingClientRect().top - getElementCssTop(el);
+            elementOriginalRelativeTop = el.offsetTop - getElementCssTop(parentEl) - getElementCssTop(el);
             viewPortHeight = document.documentElement.clientHeight;
-            containerAvailableHeight = $parentElement.height() - elementOriginalRelativeTop;
+            containerAvailableHeight = parseInt(getComputedStyle(parentEl).height) - elementOriginalRelativeTop;
             (elementHeight > viewPortHeight) ? elementHigherThanViewPort = true : elementHigherThanViewPort = false;
         }
 
         function reset() {
             initMeasures();
-            moveAside($document.scrollTop());
+            moveAside(docBody.scrollTop);
         }
 
         initMeasures();
@@ -52,8 +54,8 @@
         }
 
         function moveAside(newPos) {
-            var currentElementRelativeTop = getElementCssTop(),
-                currentElementAbsoluteTop = $element.offset().top,
+            var currentElementRelativeTop = getElementCssTop(el),
+                currentElementAbsoluteTop = el.getBoundingClientRect().top,
                 elementNewTop = newPos - elementOriginalAbsoluteTop + yOffset,
                 elementBottom, windowBottom;
 
@@ -71,21 +73,21 @@
                 }
                 if (elementNewTop + elementHeight > containerAvailableHeight) {
                     elementNewTop = containerAvailableHeight - elementHeight;
-                    $(element).addClass('bottomReached');
+                    el.classList.add('bottomReached');
                 } else {
-                    $(element).removeClass('bottomReached');
+                    el.classList.remove('bottomReached');
                 }
 
-                $element.css('top', elementNewTop);
+                el.style.top = elementNewTop;
             }
         }
 
-        $document.scroll(function () {
+        docBody.addEventListener('scroll', function () {
             
-            pos = $document.scrollTop();
+            pos = docBody.scrollTop;
 
             setTimeout(function () {
-                newPos = $document.scrollTop();
+                newPos = docBody.scrollTop;
                 if (newPos === pos) {
                     moveAside(newPos);
                 }
@@ -93,5 +95,111 @@
         });
     }
 
-    stickyElement = setup;
+    window.stickyElement = setup;
+}());
+
+
+(function () {
+    "use strict";
+
+    if (typeof $M === 'undefined') {
+        window.$M = {};
+    }
+
+    function setup(element, yOffset) {
+
+        var $document = $(document),
+            $element = $(element),
+            elementHeight, maxScroll, yOffestResult,
+            $parentElement = $element.parent(),
+            parentElementOffsetTop,
+            parentElementHeight;
+
+        function getYOffset() {
+            if (typeof (yOffset) === 'function') {
+                yOffestResult = yOffset();
+            } else {
+                yOffestResult = yOffset;
+            }
+
+            if (yOffestResult === 'static') {
+                $element.addClass('static');
+            } else {
+                $element.css('top', yOffestResult);
+                $element.removeClass('static');
+            }
+        }
+
+        function initMeasures() {
+            elementHeight = $element.outerHeight(true);
+            parentElementOffsetTop = $parentElement.offset().top;
+            parentElementHeight = $parentElement.height();
+            $element.css('width', '');
+            $element.width($element.width());  //fix x evitare cambi di larghezza al variare del tipo di posizionamento, che fa cambiare l'elemento su cui si calcola la percentuale
+
+            getYOffset();
+
+            if (parentElementHeight < (elementHeight + $element.position().top)) {
+                $parentElement.css('min-height', elementHeight + $element.position().top);
+            }
+        }
+
+        function reset() {
+            initMeasures();
+            moveAside($document.scrollTop());
+        }
+
+        initMeasures();
+
+        $parentElement.resize(function () {
+            reset();
+        });
+
+        $element.resize(function () {
+            reset();
+        });
+
+        function moveAside(newPos) {
+            getYOffset();
+
+
+            //cache off
+            //maxScroll = ($parentElement.offset().top + $parentElement.height()) - ($element.outerHeight(true) + yOffestResult);
+
+            //cache on
+            maxScroll = (parentElementOffsetTop + parentElementHeight) - (elementHeight + yOffestResult);
+
+            if (newPos > yOffestResult) {
+                $element.addClass('scrolled');
+            } else {
+                $element.removeClass('scrolled');
+            }
+
+            if (newPos > maxScroll) {
+                $element.addClass('bottomReached');
+            } else {
+                $element.removeClass('bottomReached');
+            }
+        }
+
+        $document.on('scroll', function () {
+            moveAside($document.scrollTop());
+
+            //ricontrolliamo dopo un po' altrimenti quando si scrolla velocemente verso l'alto e si arriva a 0 non parte più lo scroll e perde l'ultimo valore
+            setTimeout(function () {
+                moveAside($document.scrollTop());
+            }, 200);
+        });
+    }
+
+    function init(element, yOffset) {
+        try {
+            setup(element, yOffset);
+        }
+        catch (e) {
+            $Y.debug.errorManager(e);
+        }
+    }
+
+    $M.stickyElement = init;
 }());
